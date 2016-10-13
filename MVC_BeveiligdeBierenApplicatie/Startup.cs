0482aa26417704673ef6b7046fecbf5c267e2ef7
MichielAchten <michiel.achten@gmail.com>
2016-10-13 10:14:19 +0200

    OefeningSecurity1

---
+++ MVC_BeveiligdeBierenApplicatie/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_BeveiligdeBierenApplicatie.Startup))]
+namespace MVC_BeveiligdeBierenApplicatie
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
